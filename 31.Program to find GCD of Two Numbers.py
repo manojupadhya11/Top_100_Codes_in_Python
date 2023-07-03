ee4e48f9ae34ec4804c5ba03c1403ce57a723f87
Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>
2023-07-03 17:55:24 +0530

    Add files via upload

---
+++ 31.Program to find GCD of Two Numbers.py
@@ -0,0 +1,10 @@
+#Program to find gcd of Two Numbers
+
+
+num1 = int(input("Enter a Number 1  "))
+num2 = int(input("Enter a Number 2  "))
+gcd = 1;
+for i in range(1, min(num1, num2)):
+    if num1%i==0 and num2%i ==0:
+        gcd = i
+print("GCD of",num1,"and",num2,"is", gcd)
